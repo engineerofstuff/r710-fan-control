ec59d5ed445f20d70481567a759163f456fefbce
engineerofstuff <62352047+engineerofstuff@users.noreply.github.com>
2020-04-01 10:08:08 -0600

    Add files via upload

--- Fan_Control.rb
+++ Fan_Control.rb
@@ -5,26 +5,33 @@ require 'singleton'
 module R710_Tools
   class Fan_Control
     include Singleton
-    @@config_locations = [ '/etc/fan-control.yaml', 'fan-control.yaml' ]
+
+    # important config
+    @@config_locations = ['/etc/custom-fan-control/speeds.yaml', 'speeds.yaml']
+    @@inverval = 5            # time between checks in control loop
+    @@max_manual_temp = 81    # switch back to automatic fan control at this temp
+    @@cool_down_time = 120    # after switch to automatic wait that long before checking again
+
     @config = nil
     @is_manual = false
     @last_speed_set = 0
     @ipmitool = nil
+    @last_core_temp = 0
+
 
     # load config on init
     def initialize
       @ipmitool = `which ipmitool`.strip
-      raise "ipmitool command not found" unless File.exist? @ipmitool
+      raise 'ipmitool command not found' unless File.exist? @ipmitool
       @sensors = `which sensors`.strip
-      raise "sensors command not found" unless File.exist? @sensors
+      raise 'sensors command not found' unless File.exist? @sensors
       @@config_locations.each do |loc|
-        if File.exist?(loc)
-          puts "Loading configuration from #{loc}".colorize(:yellow)
-          @config = YAML.load_file(loc)
-          break
-        end
+        next unless File.exist?(loc)
+        # puts "Loading configuration from #{loc}".colorize(:yellow)
+        @config = YAML.load_file(loc)
+        break
       end
-      raise "Did not find config file!" if @config.nil?
+      raise 'Did not find config file!' if @config.nil?
     end
 
     # get current cpu core temperature from sensors
@@ -33,77 +40,75 @@ module R710_Tools
       max_temp = 0.0
       min_temp = 100
       output.each_line do |line|
-        if line =~ /^Core.*\+(\d+\.\d+)°C\s+\(/
-          t = $1.to_f
-          max_temp = t if t > max_temp
-          min_temp = t if t < min_temp
-        end
+        next unless line =~ /^Core.*\+(\d+\.\d+)°C\s+\(/
+        t = Regexp.last_match(1).to_f
+        max_temp = t if t > max_temp
+        min_temp = t if t < min_temp
       end
-      return { :min => min_temp, :max => max_temp}
+      { min: min_temp, max: max_temp }
     end
 
     # get current ambient temp via ipmi
     def get_ambient
-      output = `#{@ipmitool} -I lanplus -H #{@config[:host]} -U #{@config[:user]} -P #{@config[:pass]} sdr get "Ambient Temp"`
+      output = `sudo #{@ipmitool} sdr get "Ambient Temp"`
       result = {}
       output.each_line do |line|
         if line =~ /Sensor Reading\s+:\s+(\d+)/
-          result[:current] = $1.to_i
+          result[:current] = Regexp.last_match(1).to_i
           next
         end
         if line =~ /Upper critical\s+:\s+(\d+)/
-          result[:crit] = $1.to_i
+          result[:crit] = Regexp.last_match(1).to_i
           next
         end
         if line =~ /Upper non-critical\s+:\s+(\d+)/
-          result[:warn] = $1.to_i
+          result[:warn] = Regexp.last_match(1).to_i
           next
         end
         if line =~ /Status\s+:\s+(\w+)/
-          result[:status] = $1
+          result[:status] = Regexp.last_match(1)
           next
         end
       end
-      return result
+      result
     end
 
     # get current fan speeds via ipmi
     def get_fan_speed
-      output = `#{@ipmitool} -I lanplus -H #{@config[:host]} -U #{@config[:user]} -P #{@config[:pass]} sdr type Fan`
+      output = `sudo #{@ipmitool} sdr type Fan`
       max_speed = 0
-      min_speed = 15000
+      min_speed = 15_000
       output.each_line do |line|
-        if line =~ /(\d+)\s+RPM$/
-          rpm = $1.to_i
-          max_speed = rpm if rpm > max_speed
-          min_speed = rpm if rpm < min_speed
-        end
+        next unless line =~ /(\d+)\s+RPM$/
+        rpm = Regexp.last_match(1).to_i
+        max_speed = rpm if rpm > max_speed
+        min_speed = rpm if rpm < min_speed
       end
-      return { :min => min_speed, :max => max_speed}
+      { min: min_speed, max: max_speed }
     end
 
     # set the fan speed to the given percentage of max speed
     #
     # @param target fan speed in percent of max speed as integer
-    def set_fan_speed(speed_percent)
-      target_speed = sprintf("%02X",speed_percent)
-      system("#{@ipmitool} -I lanplus -H #{@config[:host]} -U #{@config[:user]} -P #{@config[:pass]} raw 0x30 0x30 0x02 0xff 0x#{target_speed}")
+    def set_fan_speed(speed_percent, cur_temp)
+      target_speed = format('%02X', speed_percent)
+      system("sudo #{@ipmitool} raw 0x30 0x30 0x02 0xff 0x#{target_speed}")
       @last_speed_set = speed_percent
-      puts "Fan speed set to #{speed_percent}% (0x#{target_speed})"
+      puts "Fan speed set to #{speed_percent}% -> CPU Temp: #{cur_temp} C".colorize(:white).bold
     end
 
     # set fan speed control to manual
     def set_fan_manual
-      system("#{@ipmitool} -I lanplus -H #{@config[:host]} -U #{@config[:user]} -P #{@config[:pass]} raw 0x30 0x30 0x01 0x00")
+      system("sudo #{@ipmitool} raw 0x30 0x30 0x01 0x00")
       @is_manual = true
-      puts "Manual fan control active".colorize(:light_blue)
+      puts 'Manual fan control active'
     end
 
     # set fan speed control to automatic
     def set_fan_automatic
-      system("#{@ipmitool} -I lanplus -H #{@config[:host]} -U #{@config[:user]} -P #{@config[:pass]} raw 0x30 0x30 0x01 0x01")
+      system("sudo #{@ipmitool} raw 0x30 0x30 0x01 0x01")
       @is_manual = false
-      puts "Automatic fan control restored".colorize(:green)
+      puts 'Automatic fan control restored'.colorize(:green)
     end
 
     # get target speed for current temperature
@@ -123,23 +128,26 @@ module R710_Tools
     def fan_control_loop
       puts 'Starting fan control loop'.colorize(:white).bold
       begin
-        while true
+        loop do
           cur_temp = get_temperature[:max]
-          if cur_temp > @config[:max_manual_temp]
-            puts "Temperature higher than max_manual_temp -> switching to automatic"
+          if cur_temp > @@max_manual_temp
+            puts 'Temperature higher than max_manual_temp -> switching to automatic'
             set_fan_automatic
-            puts "Cool down period started"
-            sleep @config[:cool_down_time]
+            puts 'Cool down period started'
+            sleep @@cool_down_time
             next
           end
-          set_fan_manual if !@is_manual
+          set_fan_manual unless @is_manual
           target = get_target_speed(cur_temp)
-          set_fan_speed(target) if target != @last_speed_set
-          sleep @config[:interval]
+          set_fan_speed(target, cur_temp) if target != @last_speed_set
+          puts "CPU Temp: #{cur_temp} C".colorize(:light_blue) if cur_temp != @last_core_temp
+          @last_core_temp = cur_temp
+          sleep @@inverval
         end
       rescue StandardError => e
-        puts "Exception or Interrupt occurred - switching back to automatic fan control"
+        puts 'Exception or Interrupt occurred - switching back to automatic fan control'
         set_fan_automatic
+
         raise e
       end
     end
--- README.md
+++ README.md
@@ -1,27 +1,69 @@
 # r710-fan-control
 
+This is a modified fork of `sulaweyo/r710-fan-control`. I really only changed some small things, nothing major.
+
+### Install
+
+1.  install software
+
+    -   ruby and ruby gem
+
+    -   `sudo apt install -y ruby ruby-devel rubygems lm_sensors ipmitool`
+    -   `gem thor colorize`
+
+2.  modify the config in `Fan_Control.rb`
+
+3.  modify the fan speeds in `speeds.yaml`. Right now the speeds are optimized to keep the temp below 73° on a 24/7 high cpu load so you may want something more conservative:
+
+         ```yaml
+
+    :speed_steps:
+              !ruby/range 0..40: 15   # run at 10% speed up to 30°C
+              !ruby/range 41..50: 20  # run at 15% speed up to 40°C
+              !ruby/range 51..55: 30  # run at 20% speed up to 50°C
+              !ruby/range 56..60: 35  # run at 30% speed up to 60°C
+              !ruby/range 61..66: 40  # run at 40% speed up to 65°C
+
+         ```
+
+
+4.  run with `ipmi-fancontrol.rb start`. The script has a help command too
+
+### Notes
+
+Be careful with this. If you mess up and the script dies or whatever the fans could be stuck at the wrong speed and your cpus can melt lol. Running this as a service will almost 100% avoid this because of the auto-restart features and `ExecStopPost`. I trust it and I had it running for months so there's really no worry unless you decide to use this in production.
+
+If you are going to use this as a service, install the file`fan-control.service`. Make sure to modify it for your specific OS.
+
+* * *
+
+# r710-fan-control
+
 If you run your R710 in a dedicated server room noise won't be an issue for you. If you have a R710 in your flat the situation is a bit different as per default the fans ramp up pretty fast and the noise level goes up with them. Mine actually never goes below 3.6k RPM which is still acceptable but the slightest increase in load will get at least some fans up to 6k RPM and that is very noticeable.
 
 ### How does it work?
 
 While searching for a solution to this problem i stumbled across some scripts by [@NoLooseEnds](https://github.com/NoLooseEnds) that set the speed to a defined value: [R710-IPMI-TEMP](https://github.com/NoLooseEnds/Scripts/tree/master/R710-IPMI-TEMP)
 
-Based on that i started playing around with the ipmitool and wrote this tool in Ruby that can be used to set a specific value but as well to actually monitor the cpu core temperature and ramp up the fans accordingly. To do that you can configure which fan speed should be applied at which temperature and just let it run. If the cpu temperature goes above the defined max temperature it will switch back to Dell's automatic mode. This will bring the temperature back down to a reasonable value and after a configurable cool down period we start again with our fan speeds. 
+Based on that i started playing around with the ipmitool and wrote this tool in Ruby that can be used to set a specific value but as well to actually monitor the cpu core temperature and ramp up the fans accordingly. To do that you can configure which fan speed should be applied at which temperature and just let it run. If the cpu temperature goes above the defined max temperature it will switch back to Dell's automatic mode. This will bring the temperature back down to a reasonable value and after a configurable cool down period we start again with our fan speeds.
 
 ### Prerequisites
+
 **In IDRAC**
 In order to read and set fan speeds idrac needs to be enabled and ipmi needs to be reachable.
 
 **In the OS**
-This tool is written in Ruby so *Ruby* and some gems are needed:
- - _thor_ for the cli
- - _colorize_ as I like color output
- 
-*ipmitool* needs to be installed and on the PATH
+This tool is written in Ruby so _Ruby_ and some gems are needed:
 
-*lm_sensors* needs to be installed and configured
+-   _thor_ for the cli
+-   _colorize_ as I like color output
+
+_ipmitool_ needs to be installed and on the PATH
+
+_lm_sensors_ needs to be installed and configured
 
 ### Configuration
+
 Check out the sample config file
 
 ```yaml
@@ -42,25 +84,25 @@ Check out the sample config file
   !ruby/range 61..65: 40  # run at 40% speed up to 65°C
 ```
 
-Obviously you can adjust all these values to your liking or add/remove speed steps. Just make sure to really test how the temperatures develop along with these values! 
+Obviously you can adjust all these values to your liking or add/remove speed steps. Just make sure to really test how the temperatures develop along with these values!
 
 ### Installation
+
 Check out the script to wherever you like and run 'bundler install' in that directory to get the required gems. After that run './Fan-Control-CLI.rb' to see available commands.
 
-```
-./Fan-Control-CLI.rb 
-Commands:
-  Fan-Control-CLI.rb fanspeed          # Get the current fan speed
-  Fan-Control-CLI.rb help [COMMAND]    # Describe available commands or one specific command
-  Fan-Control-CLI.rb reset             # Switch back to automatic fan control
-  Fan-Control-CLI.rb setspeed [value]  # Set fan speed to given percent of max speed
-  Fan-Control-CLI.rb start             # Start fan control loop
-  Fan-Control-CLI.rb temp              # Get current cpu core temperature
-```
+    ./Fan-Control-CLI.rb
+    Commands:
+      Fan-Control-CLI.rb fanspeed          # Get the current fan speed
+      Fan-Control-CLI.rb help [COMMAND]    # Describe available commands or one specific command
+      Fan-Control-CLI.rb reset             # Switch back to automatic fan control
+      Fan-Control-CLI.rb setspeed [value]  # Set fan speed to given percent of max speed
+      Fan-Control-CLI.rb start             # Start fan control loop
+      Fan-Control-CLI.rb temp              # Get current cpu core temperature
 
-To run it as a service a systemd unit is included but you need to update location and user before starting it. Copy or link it to '/etc/systemd/system/', run 'systemctl daemon-reload' to make systemd aware of the new service and then start/enable it. 
+To run it as a service a systemd unit is included but you need to update location and user before starting it. Copy or link it to '/etc/systemd/system/', run 'systemctl daemon-reload' to make systemd aware of the new service and then start/enable it.
 
 ### Disclaimer
+
 The sample values seem to work fine for me but I cannot emphasise enough that you need to test what works for you. The script disables the built in fan control and that can lead to damage!
 
-**I take NO responsibility if you mess up anything.** 
+**I take NO responsibility if you mess up anything.**
